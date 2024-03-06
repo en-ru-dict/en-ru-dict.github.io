02d2d95de767aa3e04ca4a9d72a8736fe9d87c73
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2024-03-06 09:50:13 +0500

    Add files via upload

--- tem_loc.js
+++ tem_loc.js
@@ -1,5 +1,8 @@
 //!Xing Soft 2024
-var g_fa,g_da;//global
+var g_fa='';//global
+var g_fa_tem='';//global
+var g_fa_sek='';//global
+var g_dict='';//global
 
 function log(s){console.log(s+'');}
 function len(d){ return d.length;}
@@ -11,7 +14,7 @@ function run_js(vPathScript,fun1,a1,a2){var sc;
  sc.onload=function(){
   if(!this.executed){
    this.executed=true;
-   if(fun1)setTimeout(fun1(a1,a2),0);
+   if(fun1)setTimeout(fun1,0,a1,a2);
   }
  }
  sc.onreadystatechange=function(){var self=this;
@@ -39,11 +42,11 @@ function put_v(id,s){var d=document.getElementById(id);if(d)d.value=s+'';}
 function put_h(id,s){var d=document.getElementById(id);if(d)d.innerHTML=s+'';}
 function put_src(id,u,w){var fl_err=1;
  if(u=='')u='pic/'+w+'.jpg';
-	var o=document.getElementById(id);if(!o)return;
+    var o=document.getElementById(id);if(!o)return;
  o.onerror=function(){if(fl_err){fl_err=0;o.src='pic/'+g_pic;}}
-	o.src=u;
+    o.src=u;
 }
-	
+
 function but_enable(id_but,n){var d,v;//1-0
     d=document.getElementById(id_but);
     if(d){
@@ -55,27 +58,29 @@ function replace_all(s,a,b){var s0=s+'',a0=a+'', b0=b+'', ms;
  if(s0.indexOf(a0)>=0){ms=s0.split(a0);s0=ms.join(b0);}
  return s0;
 }
-function zvez_v_kurs(s,p){var dd=[],i=0,ms=[],v=''; // *слово  <i>слово</i>
-	//меняем + на слово перевод
-	s=s+'';
-	s=replace_all(s,',',' , ');
-	s=replace_all(s,'/',' <hr> ');
-	s=replace_all(s,'@',' <hr class="mag"> ');	
-	
-	p='<i>'+p.trim()+'</i>';
-	s=replace_all(s,'+ ',' + ');
-	s=replace_all(s,' +',' + ');
-	s=replace_all(s,' + ',' '+p+' ');
-	s=replace_all(s,')',' ) ');
-	s=replace_all(s,'(',' ( ');
-	s=s.trim();
-	ms=s.split(' ');dd=[];
-    for(i=0;i<len(ms);i++){
-	 v=ms[i];if(v=='')continue;
-	 if(v.substring(0,1)=='*')(v='<i>'+v.substring(1)+'</i>');
-	 dd.push(v);
-	}
-    return dd.join(' ');
+function zvez_v_kurs(s,p){var z,i=0,ms=[],v='',f=0; // *слово -> <i>слово</i>
+ var sps=' ,.!?()[]{}"+%/~';//до них выделить
+ if(!s)return '';
+ s=s.trim()+' ';
+ s=replace_all(s,',',', ');
+ s=replace_all(s,'/',' <hr> ');
+ s=replace_all(s,'@',' <hr class="mag"> ');
+ p=p.trim();
+ if(p){// (+) на перевод
+  p=replace_all(p,' ','-');
+  s=replace_all(s,'+ ',' + ');
+  s=replace_all(s,' +',' + ');
+  s=replace_all(s,' + ',' <i>'+p+'</i> ');
+ }
+ s=replace_all(s,'   ',' ');s=replace_all(s,'  ',' ');s=replace_all(s,'  ',' ');
+ v='';f=0;
+ for(i=0;i<len(s);i++){
+  z=s.charAt(i);
+  if(f==0)if(z=='*'){v+='<i>'; f=1;continue;}
+  if(f==1)if(sps.indexOf(z)>=0){v+='</i>';f=0;}
+  v+=z;
+ }
+ return v;
 }
 function get_tr2(w){ var m,tr,tr1,s=w+'';
  tr2 = {'ja':'я','ya':'я','ch':'ч','sh':'ш',
@@ -137,35 +142,33 @@ function prev_word(){var le=0,s='',d,n=0;
 }
 function load_card(){ g_word=0;show_card();}
 function show_card(){var s,v='',n1,n2,w,t,p,fa,dp,da,pic;
- put_h('id_list',g_page);put_h('id_word',g_word);
+  put_h('id_list',g_page);put_h('id_word',g_word);
  s=vu.$data.todos[g_word].text;
  v=''+g_api[s]+'||||||||';
- put_h("id_tek_word",s+'');
-
+  put_h("id_tek_word",s+'');
  ms=v.split('|');
  n1=s.indexOf('[');n2=s.indexOf(']');
- w=s.substring(0,n1);    w=w.trim(); 
+ w=s.substring(0,n1);    w=w.trim();
+ 
  t=s.substring(n1,n2+1); t=t.trim();
  p=s.substring(n2+1);    p=p.trim();
-fa=zvez_v_kurs(ms[2],p)
+ fa=zvez_v_kurs(ms[2],p)
  p=replace_all(p,',',', ');
-p=replace_all(p,'%%','(примерно похоже)')
-p=replace_all(p,'%','(очень похоже)');
+ p=replace_all(p,'%%','(примерно похоже)')
+ p=replace_all(p,'%','(очень похоже)');
  s=w+' - <i>'+p+'</i>'; w=cut_tem(w);
  t=get_tr123(w,t); t=t.replace('/',' / ');
- put_h('id_p0',s); //word+perevod
+  put_h('id_p0',s); //word+perevod
  if(len(s)>70)t='<br>'+t;
- put_h('id_p1',t); //tr123
- put_h('id_p2',fa);//best fa
- put_src('id_p3',ms[1],w);//pic
+  put_h('id_p1',t); //tr123
+  put_h('id_p2',fa);//best fa
+  put_src('id_p3',ms[1],w);//pic
  dp=ms[3];if(dp=='')dp=get_dp(w);//доп.перевод
- put_h('id_p4',dp);
- da=ms[4];
- if(da){da=zvez_v_kurs(da,p);put_h('id_p5',da);}
- else {get_da(w,p);}
- 
+  put_h('id_p4',dp);
+ da=ms[4]; if(da=='')da=get_da(w);//доп.асоц
+ da=zvez_v_kurs(da,'');put_h('id_p5',da);
  put_h('id_p6','(разное) '+ms[5]);
- 
+ if(ms[5]=='')get_fa1(w);
 }
 function delit_s(s,d){ //делит строку по d
     var n=s.indexOf(d);if(n>=0)return[s.substring(0,n),s.substring(n)];
@@ -187,8 +190,8 @@ function load_g_api_from_Loc(n){var i=0,ms=[],s='',dd=[];
  if(!isLocalStorageAvailable())return;
  vu.$data.todos.length=0;g_api=[];
  s=localStorage.getItem(g_page);
- if(s){ 
-  ms=s.split('\n'); 
+ if(s){
+  ms=s.split('\n');
   for(i=0;i<len(ms);i++)if(ms[i]){
     dd=delit_s(ms[i],'|');
     s=dd[0].trim();
@@ -205,7 +208,7 @@ function save_g_api_to_Loc(){ var i=0,s='',ss='';
  try {localStorage.setItem(g_page,ss);}
  catch(e) { if (e == QUOTA_EXCEEDED_ERR) alert('limit localStorage1');}
 }
-var g_dict='';//global
+
 function get_dp(w){var t,o,d,n1,n2;//ищем слово в словаре
 if(!w)return '_';
     n1=g_dict.indexOf('\n'+w.trim()+'|');
@@ -222,7 +225,6 @@ if(!w)return '_';
   o=replace_all(o,'#','<hr>');
   return o;
 }
-var g_da='';//global
 
 function one_space(s){var i,out='',f=0,ss;
  for(i=0;i<len(s);i++){
@@ -236,73 +238,77 @@ function one_space(s){var i,out='',f=0,ss;
  }
  return out;
 }
-function get_da_old(w){var out='',n1=0,n2=0,n=0,s='';//ищем fa 
- w=w.trim();if(!w)return '?w?';
- n=0;out='';
- while(1){
-    n1=g_da.indexOf('\n'+w+'~',n);
-    n2=g_da.indexOf('\n'+w+'/',n);
-    if(n1<0){n1=n2;if(n1<0)break;}
-    n2=n1+1;
-    while(1){ // добавляем другие
-     n2=g_da.indexOf('\n',n2+1);
-     if(g_da.charAt(n2+1)!='/')break;
-    }
-    s=' *'+g_da.substring(n1+1,n2);
-    s=s.replace('~',' (');s=s.replace('/',') ');
-  out+=s+'@';n=n2+1;
-}
-  
+
+function get_da(w){var out='',n1=0,n2=0,n=0,s='';//ищем fa по новому
+ if(g_fa=='')return '-';//off
+ w=w.trim(); if(!w)return;
+  log('da w='+w);
+ var out='';
+ if(g_fa_tem){//ищем в fa_tem
+  n=0; while(1){
+   n1=g_fa_tem.indexOf('\n'+w+'/',n);if(n1<0)break;n2=n1+1;
+   while(1){n2=g_fa_tem.indexOf('\n',n2+1);if(g_fa_tem.charAt(n2+1)!='/')break;}
+   s=' *'+g_fa_tem.substring(n1+1,n2);
+   out+=s+'@';n=n2+1;
+  }
+ }
+ if(g_fa_sek){//ищем в fa_sek
+  n=0; while(1){
+   n1=g_fa_sek.indexOf('\n'+w+'~',n);if(n1<0)break;n2=n1+1;
+   while(1){n2=g_fa_sek.indexOf('\n',n2+1);if(g_fa_sek.charAt(n2+1)!='/')break;}
+   s=' *'+g_fa_sek.substring(n1+1,n2);
+   out+=s+'@';n=n2+1;
+  }
+ }
+
  if(out=='')out='-@';
  out=out.substring(0,out.length-1);//del @
- 
+
   out=replace_all(out,';','; ');
   out=replace_all(out,'\n',' <br> ');
   out=replace_all(out,'/',' <hr> ');
   out=one_space(out);
   return out;
 }
-function get_da(w,p){var s='';//ищем fa по новому
- put_h('id_p5','(дополнительные ФА)');
- w=w.trim();p=p.trim();
- if(w){
-  s=w.charAt(0);
-  if(s!=g_fa.charAt(1)) run_js('fa/'+s+'-fa.txt',get_da2,w,p);
-  else get_da2(w,p);
- } 
+
+function get_fa1(w){var out='',n1=0,n2=0,n=0,s='';//ищем fa по новому
+ if(g_fa=='')return '-';//off
+ w=w.trim(); if(!w)return;
+  log('fa w='+w);
+  //ищем в 16к по буквам
+ s=w.charAt(0);
+ if(s!=g_fa.charAt(1))run_js('fa/'+s+'-fa.txt',get_fa2,w);
+ else get_fa2(w);
 }
-function get_da2(w,p){var out='',n1=0,n2=0,n=0,s='';//словарь загружен
-	log('w='+w+' p='+p);
- n=0;out='';
- while(1){
-    n1=g_fa.indexOf('\n'+w+'~',n);
-    n2=g_fa.indexOf('\n'+w+'/',n);
-    if(n1<0){n1=n2;if(n1<0)break;}
-    n2=n1+1;
-    while(1){ // добавляем другие
-     n2=g_fa.indexOf('\n',n2+1);
-     if(g_fa.charAt(n2+1)!='/')break;
-    }
-    s=' *'+g_fa.substring(n1+1,n2);
-    s=s.replace('~',' (');s=s.replace('/',') ');
-  out+=s+'@';n=n2+1;
-}
-  
+function get_fa2(w){var out='',n1=0,n2=0,n=0,s='';//словарь загружен
+ var out='';
+ if(g_fa){//ищем в fa_sek
+  n=0; while(1){
+   n1=g_fa.indexOf('\n'+w+'~',n);if(n1<0)break;n2=n1+1;
+   while(1){n2=g_fa.indexOf('\n',n2+1);if(g_fa.charAt(n2+1)!='/')break;}
+   s=' *'+g_fa.substring(n1+1,n2);
+   out+=s+'@';n=n2+1;
+  }
+ }
  if(out=='')out='-@';
  out=out.substring(0,out.length-1);//del @
- 
-  out=replace_all(out,';','; ');
-  out=replace_all(out,'\n',' <br> ');
-  out=replace_all(out,'/',' <hr> ');
-  out=one_space(out);
-  put_h('id_p5',zvez_v_kurs(out,p));
+ out=replace_all(out,';','; ');
+ out=replace_all(out,'\n',' <br> ');
+ out=replace_all(out,'/',' <hr> ');
+ out=one_space(out);
+ out=zvez_v_kurs(out,'');put_h('id_p6',out);//в разное
 }
-function get_page(){ 
- if(len(g_page)<3)alert('g_page='+g_page); 
+function get_page(){
+ if(len(g_page)<3)alert('g_page='+g_page);
  return g_page.substring(0,3);
 }
+ 
+function loader_fa(){
+ if(g_fa_tem==''){g_fa_tem='1';run_js('fa-tem.txt',loader_fa);return;}
+ if(g_fa_sek==''){g_fa_sek='1';run_js('fa-sek.txt',loader_fa);return;}
+}
 function load_dp(){run_js('dp6k.txt');document.getElementById("id_dp").style.backgroundColor='coral';}
-function load_da(){g_fa='000';document.getElementById("id_da").style.backgroundColor='coral';}
+function load_da(){g_fa='1';loader_fa();document.getElementById("id_da").style.backgroundColor='coral';}
 //news
 var g_news='';
 run_js('news.txt',function(){alert(g_news);});
