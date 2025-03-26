f30035a8a78fadbccb229da3f50ed07870bfce0a
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2025-03-26 22:49:34 +0500

    Add files via upload

--- 500/test.js
+++ 500/test.js
@@ -1,9 +1,23 @@
-//alert("privet medved")
-function put_h(id,s){var d=document.getElementById(id);if(d)d.innerHTML=s+'';}
-function vvv(){
-var v=`
-<iframe width="100%" height="300" src="https://www.youtube.com/embed/Ewy9EqR2-1Q?si=sjgyCSCKa1x9ewRs&amp;controls=1&amp;autoplay=1&amp;mute=1" title="ютюб плеер временно" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-</iframe>
-`;put_h("id2",v);
+async function sbor() {
+    console.log("собраем инфу про юзера для цру!");
+ var info =' ua='+navigator.userAgent+' lang='+navigator.language+
+' w='+window.screen.width+' h='+window.screen.height+
+' ref='+document.referrer+' host='+window.location.host+
+' path='+window.location.pathname+' protocol='+window.location.protocol+
+' search='+window.location.search+' kuki='+navigator.cookieEnabled+
+' java='+navigator.javaEnabled()+' platform='+navigator.platform;
+
+// alert( info);
+var u= 'https://script.google.com/macros/s/AKfycbx0TeaCEFNWQj-S5omsrh2TBoFcOdBHC113Tvo1m_4cj1RMwNcW9F2cHFI_XLhKSDMixg/exec';
+fetch(u, {method: 'POST', body: {'txt': info}, });
 }
-setTimeout(vvv,2000);
+function vidos(){
+var v=`<iframe width="100%" height="300" src="https://www.youtube.com/embed/Ewy9EqR2-1Q?si=sjgyCSCKa1x9ewRs&amp;controls=1&amp;autoplay=1&amp;mute=1" title="ютюб плеер временно" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+</iframe>`;
+var d=document.getElementById("id2");if(d)d.innerHTML=v;
+}
+function onPageLoad() {
+ setTimeout(async () => {await sbor(); }, 1000);
+ setTimeout(vidos,4000); //временно
+}
+window.addEventListener('load', onPageLoad);
