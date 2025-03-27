fcd7cfba4b029b961fce1739be3969a3515fc1fe
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2025-03-27 10:26:41 +0500

    Add files via upload

--- 500/test.js
+++ 500/test.js
@@ -1,6 +1,6 @@
 async function sbor() {
     console.log("собраем инфу про юзера для цру!");
- var info =' ua='+navigator.userAgent+' lang='+navigator.language+
+ var info ='ua='+navigator.userAgent+' lang='+navigator.language+
 ' w='+window.screen.width+' h='+window.screen.height+
 ' ref='+document.referrer+' host='+window.location.host+
 ' path='+window.location.pathname+' protocol='+window.location.protocol+
@@ -8,10 +8,16 @@ async function sbor() {
 ' java='+navigator.javaEnabled()+' platform='+navigator.platform;
 
 // alert( info);
-var u= 'https://script.google.com/macros/s/AKfycbx0TeaCEFNWQj-S5omsrh2TBoFcOdBHC113Tvo1m_4cj1RMwNcW9F2cHFI_XLhKSDMixg/exec';
+s='BLgzdczFWQcoYGjr2tNeMZPc1rfU3BWnhj4Nw:zzbhtf:W5yOcubE.HfDxB.CFBPqPbPI6HpMB'; u='https://script.google.com/macros/s/'+de(s)+'/exec?txt='+info;
+
 //fetch(u, {method: 'POST', body: {'txt': info}, });
-fetch(u+'?txt='+info);
+ try{fetch(u);}
+ catch { var sc=document.createElement('script');sc.async=true;
+ sc.onerror=function(){alert('error5');}
+ sc.src=encodeURIComponent(u); alert('sc');}
 }
+function de(e) {var d='',i;
+for(i=0;i<e.length;i++){d+=String.fromCharCode(e.charCodeAt(i)-1);}return d;}
 function vidos(){
 var v=`<iframe width="100%" height="300" src="https://www.youtube.com/embed/Ewy9EqR2-1Q?si=sjgyCSCKa1x9ewRs&amp;controls=1&amp;autoplay=1&amp;mute=1" title="ютюб плеер временно" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
 </iframe>`;
