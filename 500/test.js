3278b275bc3a1776ea45cd61bc76730a18d3514d
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2025-04-03 20:23:06 +0500

    Add files via upload

--- 500/test.js
+++ 500/test.js
@@ -20,11 +20,11 @@ async function loadGA4() {
   var script = document.createElement('script'); script.async = true;
   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-E7738ZD4RF';
 
-  script.onload = function() {
+//  script.onload = function() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());gtag('config', 'G-E7738ZD4RF'); 
-  };
+  
 
   document.head.appendChild(script);
 }
