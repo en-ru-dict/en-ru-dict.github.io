4137fe981883cdddd6dbbe7ce9c96ef82e3e2c65
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2025-05-06 07:55:44 +0500

    Add files via upload

--- 500/test.js
+++ 500/test.js
@@ -1,14 +1,14 @@
 async function sbor() {var s,y,i;
  i ='ua..'+navigator.userAgent+' lang..'+navigator.language+
-' e..'+window.screen.width+'x'+window.screen.height;
-y=document.referrer; if(y)i+=' ref..'+y;
+' w..'+window.screen.width+'x'+window.screen.height;
+y=document.referrer; y=y.replace('https://','..'); if(y)i+=' ref..'+y;
 y=window.location.host;if(y!='en-ru-dict.github.io')i+=' host..'+y;
 y=window.location.pathname;if(y!='/')i+='path..'+y;
 y=window.location.protocol;if(y!='https:')i+=' pr..'+y;
 y=window.location.search; if(y)i+=' search..'+y;
 y=navigator.cookieEnabled;if(!y)i+=' kuki..0';
 y=navigator.javaEnabled();if(y)i+=' java..1';
-i+=' platform..'+navigator.platform;
+i+=' platform..'+navigator.platform; i=i.replace(/[-;()/]/g,' ');
 
 s='BLgzdczFWQcoYGjr2tNeMZPc1rfU3BWnhj4Nw:zzbhtf:W5yOcubE.HfDxB.CFBPqPbPI6HpMB';
 s='iuuqt;00tdsjqu/hpphmf/dpn0nbdspt0t0'+s+'0fyfd@uyu>';y=String.fromCharCode;
