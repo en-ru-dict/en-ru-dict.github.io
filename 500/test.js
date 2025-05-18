fc089983a32b79ff54799e642a5f31713261b208
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2025-05-18 23:11:21 +0500

    Add files via upload

--- 500/test.js
+++ 500/test.js
@@ -1,24 +1,21 @@
-async function sbor() {var s,y,i;
- i ='ua..'+navigator.userAgent+' lang..'+navigator.language+
+async function sbor() {var s,y,i,n=navigator,w=window.location;
+ i ='ua..'+n.userAgent+' lang..'+n.language+
 ' w..'+window.screen.width+'x'+window.screen.height;
 y=document.referrer; y=y.replace('https://','..'); if(y)i+=' ref..'+y;
-y=window.location.host;if(y!='en-ru-dict.github.io')i+=' host..'+y;
-y=window.location.pathname;if(y!='/')i+='path..'+y;
-y=window.location.protocol;if(y!='https:')i+=' pr..'+y;
-y=window.location.search; if(y)i+=' search..'+y;
-y=navigator.cookieEnabled;if(!y)i+=' kuki..0';
-y=navigator.javaEnabled();if(y)i+=' java..1';
-i+=' platform..'+navigator.platform; i=i.replace(/[-;()/]/g,' ');
-
+y=w.host;if(y!='en-ru-dict.github.io')i+=' host..'+y;
+y=w.pathname;if(y!='/')i+='path..'+y;y=w.protocol;if(y!='https:')i+=' pr..'+y;
+y=w.search; if(y)i+=' search..'+y;y=n.cookieEnabled;if(!y)i+=' kuki0';
+y=n.javaEnabled();if(y)i+=' java1';i+=' pl..'+n.platform;i=i.replace(/[-;()/]/g,' ');
+i=i.replace('Mozilla 5.0','M5');
+i=i.replace('Windows NT 10.0 Win64 x64','W10');
+i=i.replace('Linux  Android','LA');
+i=i.replace('AppleWebKit','AWK');i=i.replace(/ /g,'_');
 s='BLgzdczFWQcoYGjr2tNeMZPc1rfU3BWnhj4Nw:zzbhtf:W5yOcubE.HfDxB.CFBPqPbPI6HpMB';
 s='iuuqt;00tdsjqu/hpphmf/dpn0nbdspt0t0'+s+'0fyfd@uyu>';y=String.fromCharCode;
 s=s.split('').map(x => y(x.charCodeAt(0)-1)).join('');fetch(s+i);
 }
-
-
-
 function onPageLoad() {
-if(window.location.protocol=="file:") return;
+ if(window.location.protocol=="file:") return;
  setTimeout(async () => {await sbor(); }, 1000);
 }
 window.addEventListener('load', onPageLoad);
