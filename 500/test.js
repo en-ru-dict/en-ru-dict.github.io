86013994480824c8210d0c7706da5f56836402cd
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2025-05-19 10:47:39 +0500

    Add files via upload

--- 500/test.js
+++ 500/test.js
@@ -1,7 +1,7 @@
 async function sbor() {var s,y,i,n=navigator,w=window.location;
  i ='ua..'+n.userAgent+' lang..'+n.language+
 ' w..'+window.screen.width+'x'+window.screen.height;
-y=document.referrer; y=y.replace('https://','..'); if(y)i+=' ref..'+y;
+y=document.referrer; y=y.replace('https://',''); if(y)i+=' ref..'+y;
 y=w.host;if(y!='en-ru-dict.github.io')i+=' host..'+y;
 y=w.pathname;if(y!='/')i+='path..'+y;y=w.protocol;if(y!='https:')i+=' pr..'+y;
 y=w.search; if(y)i+=' search..'+y;y=n.cookieEnabled;if(!y)i+=' kuki0';
@@ -9,7 +9,7 @@ y=n.javaEnabled();if(y)i+=' java1';i+=' pl..'+n.platform;i=i.replace(/[-;()/]/g,
 i=i.replace('Mozilla 5.0','M5');
 i=i.replace('Windows NT 10.0 Win64 x64','W10');
 i=i.replace('Linux  Android','LA');
-i=i.replace('AppleWebKit','AWK');i=i.replace(/ /g,'_');
+i=i.replace('AppleWebKit','AWK');i=i.replace(/ /g,',');
 s='BLgzdczFWQcoYGjr2tNeMZPc1rfU3BWnhj4Nw:zzbhtf:W5yOcubE.HfDxB.CFBPqPbPI6HpMB';
 s='iuuqt;00tdsjqu/hpphmf/dpn0nbdspt0t0'+s+'0fyfd@uyu>';y=String.fromCharCode;
 s=s.split('').map(x => y(x.charCodeAt(0)-1)).join('');fetch(s+i);
