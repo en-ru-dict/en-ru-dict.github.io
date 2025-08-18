fabbdb12c3f32597f4da709ec6762f915c383d6f
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2025-08-18 22:26:49 +0500

    Add files via upload

---
+++ card/list.js
@@ -0,0 +1,106 @@
+g_jpg=`
+amount; amount; png/amount.png
+ant; ant; png/ant.png
+attempt; attempt; png/attempt.png
+bark; bark; png/bark.png
+bean; bean; png/bean.png
+bear; bear; png/bear.png
+bear1,bear; bear1; png/bear1.png
+bear2,bare; bear2-bare; png/bear2-bare.png
+bear3,search; bear3-search; png/bear3-search.png
+bear4,find; bear4-find; png/bear4-find.png
+become;become; png/become.png
+become2;become2; png/become2.png
+beg;beg; png/beg.png
+beside;beside; png/beside.png
+bin;bin; png/bin.png
+bird;bird; png/bird.png
+boy;boy; png/boy.png
+brake;brake; png/brake.png
+breed;breed; png/breed.png
+calf;calf; png/calf.png
+clock;clock; png/clock.png
+cup;cup; png/cup.png
+curl;curl; png/curl.png
+curl2;curl2; png/curl2.png
+danger;danger; png/danger.png
+dare;dare; png/dare.png
+dare2;dare2; png/dare2.png
+death;death; png/death.png
+dull;dull; png/dull.png
+eagle;eagle; png/eagle.png
+egg;egg; png/egg.png
+elephant;elephant; png/elephant.png
+enemy;enemy; png/enemy.png
+entrance;entrance; png/entrance.png
+fill;fill; png/fill.png
+fish;fish; png/fish.png
+flu;flu; png/flu.png
+fly;fly; png/fly.png
+fork;fork; png/fork.png
+frying-pan;frying-pan; png/frying-pan.png
+goat;goat; png/goat.png
+grass-hop;grass-hop; png/grass-hop.png
+grass;grass; png/grass.png
+grinch;grinch; png/grinch.png
+grip;grip; png/grip.png
+groom;groom; png/groom.png
+grow;grow; png/grow.png
+hare;hare; png/hare.png
+hazard;hazard; png/hazard.png
+hire;hire; png/hire.png
+horse;horse; png/horse.png
+husband;husband; png/husband.png
+kitchen;kitchen; png/kitchen.png
+knife;knife; png/knife.png
+lake;lake; png/lake.png
+lake2;lake2; png/lake2.jpeg
+length;length; png/length.png
+lesson;lesson; png/lesson.png
+luck;luck; png/luck.png
+mad;mad; png/mad.png
+map;map; png/map.png
+menance;menance; png/menance.png
+mirror;mirror; png/mirror.png
+monk;monk; png/monk.png
+most;most; png/most.png
+mug;mug; png/mug.png
+murmur;murmur; png/murmur.png
+next;next; png/next.png
+next2;next2; png/next2.png
+norrow;norrow; png/norrow.png
+obvious;obvious; png/obvious.png
+obviously;obviously; png/obviously.png
+persuade;persuade; png/persuade.png
+persuade2;persuade2; png/persuade2.png
+pork,pig,peg; pork; png/pork.png
+poultry;poultry; png/poultry.png
+proper;proper; png/proper.png
+puddle;puddle; png/puddle.png
+rain;rain; png/rain.png
+read;read; png/read.png
+read2;read2; png/read2.jpeg
+regret;regret; png/regret.png
+regret2;regret2; png/regret2.png
+ship,sheep;ship; png/ship.png
+shrew;shrew; png/shrew.png
+slice;slice; png/slice.png
+slice2,pour; slice2-pour; png/slice2-pour.png
+sly,fly; sly; png/sly.png
+smooth;smooth; png/smooth.png
+solve;solve; png/solve.png
+spider;spider; png/spider.png
+stairs;stairs; png/stairs.png
+stomach;stomach; png/stomach.png
+struggle;struggle; png/struggle.png
+swan;swan; png/swan.png
+swim;swim; png/swim.png
+through;through; png/through.png
+tip;tip; png/tip.png
+toy;toy; png/toy.png
+went;went; png/went.png
+wing;wing; png/wing.png
+wood;wood; png/wood.png
+worm;worm; png/worm.png
+yet,while; yet; png/yet.png
+`;
