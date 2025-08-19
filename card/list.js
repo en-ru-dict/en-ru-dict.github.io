f6828983ddbff568ba7b7d6475e9f1ca50077a5a
en-ru-dict <149561556+en-ru-dict@users.noreply.github.com>
2025-08-19 18:51:51 +0500

    Add files via upload

--- card/list.js
+++ card/list.js
@@ -1,106 +1,106 @@
 g_jpg=`
-amount; amount; png/amount.png
-ant; ant; png/ant.png
-attempt; attempt; png/attempt.png
-bark; bark; png/bark.png
-bean; bean; png/bean.png
-bear; bear; png/bear.png
-bear1,bear; bear1; png/bear1.png
-bear2,bare; bear2-bare; png/bear2-bare.png
-bear3,search; bear3-search; png/bear3-search.png
-bear4,find; bear4-find; png/bear4-find.png
-become;become; png/become.png
-become2;become2; png/become2.png
-beg;beg; png/beg.png
-beside;beside; png/beside.png
-bin;bin; png/bin.png
-bird;bird; png/bird.png
-boy;boy; png/boy.png
-brake;brake; png/brake.png
-breed;breed; png/breed.png
-calf;calf; png/calf.png
-clock;clock; png/clock.png
-cup;cup; png/cup.png
-curl;curl; png/curl.png
-curl2;curl2; png/curl2.png
-danger;danger; png/danger.png
-dare;dare; png/dare.png
-dare2;dare2; png/dare2.png
-death;death; png/death.png
-dull;dull; png/dull.png
-eagle;eagle; png/eagle.png
-egg;egg; png/egg.png
-elephant;elephant; png/elephant.png
-enemy;enemy; png/enemy.png
-entrance;entrance; png/entrance.png
-fill;fill; png/fill.png
-fish;fish; png/fish.png
-flu;flu; png/flu.png
-fly;fly; png/fly.png
-fork;fork; png/fork.png
-frying-pan;frying-pan; png/frying-pan.png
-goat;goat; png/goat.png
-grass-hop;grass-hop; png/grass-hop.png
-grass;grass; png/grass.png
-grinch;grinch; png/grinch.png
-grip;grip; png/grip.png
-groom;groom; png/groom.png
-grow;grow; png/grow.png
-hare;hare; png/hare.png
-hazard;hazard; png/hazard.png
-hire;hire; png/hire.png
-horse;horse; png/horse.png
-husband;husband; png/husband.png
-kitchen;kitchen; png/kitchen.png
-knife;knife; png/knife.png
-lake;lake; png/lake.png
-lake2;lake2; png/lake2.jpeg
-length;length; png/length.png
-lesson;lesson; png/lesson.png
-luck;luck; png/luck.png
-mad;mad; png/mad.png
-map;map; png/map.png
-menance;menance; png/menance.png
-mirror;mirror; png/mirror.png
-monk;monk; png/monk.png
-most;most; png/most.png
-mug;mug; png/mug.png
-murmur;murmur; png/murmur.png
-next;next; png/next.png
-next2;next2; png/next2.png
-norrow;norrow; png/norrow.png
-obvious;obvious; png/obvious.png
-obviously;obviously; png/obviously.png
-persuade;persuade; png/persuade.png
-persuade2;persuade2; png/persuade2.png
-pork,pig,peg; pork; png/pork.png
-poultry;poultry; png/poultry.png
-proper;proper; png/proper.png
-puddle;puddle; png/puddle.png
-rain;rain; png/rain.png
-read;read; png/read.png
-read2;read2; png/read2.jpeg
-regret;regret; png/regret.png
-regret2;regret2; png/regret2.png
-ship,sheep;ship; png/ship.png
-shrew;shrew; png/shrew.png
-slice;slice; png/slice.png
-slice2,pour; slice2-pour; png/slice2-pour.png
-sly,fly; sly; png/sly.png
-smooth;smooth; png/smooth.png
-solve;solve; png/solve.png
-spider;spider; png/spider.png
-stairs;stairs; png/stairs.png
-stomach;stomach; png/stomach.png
-struggle;struggle; png/struggle.png
-swan;swan; png/swan.png
-swim;swim; png/swim.png
-through;through; png/through.png
-tip;tip; png/tip.png
-toy;toy; png/toy.png
-went;went; png/went.png
-wing;wing; png/wing.png
-wood;wood; png/wood.png
-worm;worm; png/worm.png
-yet,while; yet; png/yet.png
+amount,mount; amount; https://files.catbox.moe/7g1jmu.png; 1.2 MB
+ant,aunt; ant; https://files.catbox.moe/98pmjd.png; 1.6 MB
+attempt; attempt; https://files.catbox.moe/k55efi.png; 1.4 MB
+bark; bark; https://files.catbox.moe/t5x2zw.png; 1.2 MB
+bean; bean; https://files.catbox.moe/7p53w0.png; 2.7 MB
+bear; bear; https://files.catbox.moe/i4b9gq.png; 0.3 MB
+bear1;bear1; https://files.catbox.moe/6r5heq.png; 3.4 MB
+bear2,bare; bear2-bare; https://files.catbox.moe/z1m7n1.png; 3.5 MB
+bear3,search,look-for; bear3-search; https://files.catbox.moe/knvudx.png; 2.5 MB
+bear4,find; bear4-find; https://files.catbox.moe/65dl5x.png; 3.1 MB
+become; become; https://files.catbox.moe/cunblw.png; 0.4 MB
+become2;become2; https://files.catbox.moe/m971ny.png;1.5 MB
+beg,big; beg; https://files.catbox.moe/0hwclb.png; 1.5 MB
+beside,because; beside; https://files.catbox.moe/31yjvv.png; 1.2 MB
+bin; bin; https://files.catbox.moe/0xut5o.png; 2.2 MB
+bird; bird; https://files.catbox.moe/lgynjo.png; 1.2 MB
+girl,cow,boy; boy; https://files.catbox.moe/f2ya62.png; 1.6 MB
+brake,break; brake; https://files.catbox.moe/uahr5q.png; 1.4 MB
+breed; breed; https://files.catbox.moe/7mqyjm.png; 0.9 MB
+calf; calf; https://files.catbox.moe/p3h6os.png; 2.2 MB
+clock,gear; clock; https://files.catbox.moe/ucn3ta.png; 1 MB
+cup,cap; cup; https://files.catbox.moe/c0g377.png; 1.2 MB
+curl; curl; https://files.catbox.moe/yp7myw.png; 2 MB
+curl,curly; curl2; https://files.catbox.moe/so9eqf.png; 1.1 MB
+danger; danger; https://files.catbox.moe/upfbu5.png; 1.2 MB
+dare; dare; https://files.catbox.moe/qoapmm.png; 1 MB
+dare2; dare2; https://files.catbox.moe/5l1c5q.png; 1.1 MB
+death; death; https://files.catbox.moe/rqkxyp.png; 1.5 MB
+dull,doll; dull; https://files.catbox.moe/jctbvb.png; 1.1 MB
+eagle,eager; eagle; https://files.catbox.moe/5q6kz5.png; 1.4 MB
+egg; egg; https://files.catbox.moe/i8cp3d.png; 1.3 MB
+elephant; elephant; https://files.catbox.moe/np6kym.png; 2.3 MB
+enemy,animal; enemy; https://files.catbox.moe/fnbc2o.png; 1.7 MB
+entrance,exit; entrance; https://files.catbox.moe/rhpiog.png; 1.3 MB
+fill,feel; fill; https://files.catbox.moe/1plgzz.png; 1.3 MB
+fish; fish; https://files.catbox.moe/6fg754.png; 1.9 MB
+flu,fly,sly;flu; https://files.catbox.moe/ar66v4.png; 0.6 MB
+fly,sly,slay; fly; https://files.catbox.moe/vefe3k.png; 0.9 MB
+fork,cattle,spoon; fork; https://files.catbox.moe/i9gxu2.png; 1.2 MB
+frying-pan,skillet; frying-pan; https://files.catbox.moe/449nx4.png; 0.8 MB
+goat; goat; https://files.catbox.moe/mnqy7z.png; 2.5 MB
+grass,green; grass; https://files.catbox.moe/emyk6r.png; 2.7 MB
+grass-hopper; grass-hop; https://files.catbox.moe/x9g7qt.png; 1.1 MB
+grin,grim,inch; grinch; https://files.catbox.moe/rkypq8.png; 2.1 MB
+grip,mushroom; grip; https://files.catbox.moe/whoe8g.png; 3.6 MB
+groom,gloom; groom; https://files.catbox.moe/zqvcad.png;  0.3 MB
+grow; grow; https://files.catbox.moe/gglac6.png; 1.2 MB
+hare,fare; hare; https://files.catbox.moe/q3cwpn.png; 3.9 MB
+hazard,lizard; hazard; https://files.catbox.moe/g0scuy.png; 0.9 MB
+hire,hare,hair; hire; https://files.catbox.moe/2vshwr.png; 3.4 MB
+horse; horse; https://files.catbox.moe/w4dfzq.png; 2.4 MB
+husband; husband; https://files.catbox.moe/seejgs.png; 1 MB
+kitchen,kitten; kitchen; https://files.catbox.moe/fnt2uk.png; 0.9 MB
+knife,knave; knife; https://files.catbox.moe/r9ne19.png; 1.2 MB
+lake,leak; lake; 0.png
+lake2; lake2; 0.png
+length; length; 0.png
+lesson,listen; lesson; 0.png
+luck; luck; 0.png
+mad,mud,mood; mad; 0.png
+map; map; 0.png
+menace; menace; 0.png
+mirror; mirror; 0.png
+monk,monkey; monk; 0.png
+most,majority; most; 0.png
+mug; mug; 0.png
+murmur; murmur; 0.png
+next; next; 0.png
+next2; next2; 0.png
+norrow; norrow; 0.png
+obvious; obvious; 0.png
+obviously; obviously; 0.png
+persuade; persuade; 0.png
+persuade2; persuade2; 0.png
+pig,peg,pork; pork; 0.png
+poultry; poultry; 0.png
+proper,property; proper; 0.png
+puddle,paddle; puddle; 0.png
+rain; rain; 0.png
+read,rid; read; 0.png
+read2; read2; 0.png
+regret; regret; 0.png
+regret2; regret2; 0.png
+ship,sheep; ship; 0.png
+shrew; shrew; 0.png
+slice; slice; 0.png
+slice2,pour; slice2-pour; 0.png
+slay,sly,fly; sly; 0.png
+smooth; smooth; 0.png
+decide,solve; solve; 0.png
+spider,speed; spider; 0.png
+stairs,ladder; stairs; 0.png
+stomach; stomach; 0.png
+struggle; struggle; 0.png
+swan; swan; 0.png
+swim; swim; 0.png
+through; through; 0.png
+tip; tip; 0.png
+toy; toy; 0.png
+go,went,gone; went; 0.png
+wing; wing; 0.png
+would,wood; wood; 0.png
+worm; worm; 0.png
+while,yet; yet; 0.png
 `;
